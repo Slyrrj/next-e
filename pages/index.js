29ec5e9fed750f358f1bb1fb2e9df74429675821
slyrj <slyrj@hotmail.fr>
2022-04-23 23:34:39 +0200

    Ajout Listage  produits

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
 import Layout from '../components/Layout';
+import data from '../utils/data';
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product1</li>
-          <li>Product2</li>
-          <li>Product3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>{product.price}€</Typography>
+                  <Button size="small" color="primary">
+                    Ajouter au panier
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
